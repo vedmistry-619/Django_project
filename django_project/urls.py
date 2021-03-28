1dd425d579f54cce9d964564aede1b0dc695d8c3
Ved Mistry <59229430+vedmistry-619@users.noreply.github.com>
2021-03-28 19:15:20 +0530

    first commit

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from users import views as u_views
 from django.contrib.auth import views as a_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +26,12 @@ urlpatterns = [
     path('register/',u_views.register, name ='register'),
     path('login/',a_views.LoginView.as_view(template_name = 'users/login.html'), name ='login'),
     path('logout/',a_views.LogoutView.as_view(template_name = 'users/logout.html'), name ='logout'),
+    path('password-reset/',a_views.PasswordResetView.as_view(template_name = 'users/password_reset.html'), name ='password_reset'),
+    path('password-reset/done',a_views.PasswordResetDoneView.as_view(template_name = 'users/password_reset_done.html'), name ='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',a_views.PasswordResetConfirmView.as_view(template_name = 'users/password_reset_confirm.html'), name ='password_reset_confirm'),
+    path('password-reset-complete/',a_views.PasswordResetCompleteView.as_view(template_name = 'users/password_reset_complete.html'), name ='password_reset_complete'),
     path('profile/',u_views.profile, name ='profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
--- footballnews/urls.py
+++ footballnews/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='footballnews-home'),
-    path('info',views.info,name='footballnews-information')
+    path('', PostListView.as_view(), name='footballnews-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('info/',views.info,name='footballnews-information')
 ]
